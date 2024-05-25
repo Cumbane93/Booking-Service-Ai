4b174e0202a63cd0ec42db73dd03ed372248464f
Dennis Cumbane <hello@makeit-media.de>
2024-05-25 10:23:30 +0200

    added agent card on dashboard

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,13 +1,42 @@
 import { AgentCategories } from "@/components/agent-categories";
+import { Agents } from "@/components/agents";
 import { SearchInput } from "@/components/search-input";
 import prismadb from "@/lib/prismadb";
 
-const RootPage = async () => {
+interface RootPageProps {
+    searchParams: {
+        agentCategoryId: string;
+        name: string;
+    }
+}
+
+const RootPage = async ({
+    searchParams
+}: RootPageProps) => {
+    const data = await prismadb.agent.findMany({
+        where: {
+            agentCategoryId: searchParams.agentCategoryId,
+            name: {
+                search: searchParams.name
+            }
+        },
+        orderBy: {
+            createdAt: "desc",
+        },
+        include: {
+            _count: {
+                select: {
+                    messages: true
+                }
+            }
+        }
+    })
     const agentCategories = await prismadb.agentCategory.findMany();
     return (
         <div className="h-full p-4 space-y-2">
             <SearchInput />
-            <AgentCategories data={agentCategories}/>
+            <AgentCategories data={agentCategories} />
+            <Agents  data={data} />
         </div>);
 }
 
